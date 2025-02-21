d9d59a8cbf1c6001a27b910bfd425a229cb1e335
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2025-02-20 18:53:27 -0800

    added International

---
+++ module-10/InternationalDivision.java
@@ -0,0 +1,21 @@
+// InternationalDivision class extends Division
+public class InternationalDivision extends Division {
+    private String country;
+    private String language;
+
+    // Constructor for InternationalDivision
+    public InternationalDivision(String divisionName, int accountNumber, String country, String language) {
+        super(divisionName, accountNumber);
+        this.country = country;
+        this.language = language;
+    }
+
+    // Implementation of the abstract display method
+    @Override
+    public void display() {
+        System.out.println("International Division: " + divisionName);
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Country: " + country);
+        System.out.println("Language: " + language);
+    }
+}
